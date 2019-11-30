473802a11bdc751dd030fa562522d15702d4b449
aromero45 <alex4543@hotmail.com>
2019-11-30 11:51:36 -0500

    Correccion status hijos

--- app/services/andrMutation.srv.js
+++ app/services/andrMutation.srv.js
@@ -34,34 +34,55 @@ module.exports.generateAndrMutation = function(req,success,error){
 
     for(var i = 0,p = Promise.resolve();i<itemsEx;i++){
         p= p.then(_ => new Promise(resolve => {
+        if(itemsEx>1){
             code = `${codeinit}_${item}`;
+        }else{
+            code = `${codeinit}`;
+        }
+        requestcall(path,code,req,codeinit,itemsEx).then(()=>{
             shell.exec('cd MDroidPlus && mvn clean && mvn package');
-            console.log(process.cwd())
-            shell.exec(`cd MDroidPlus/target && java -jar MDroidPlus-1.0.0.jar ../libs4ast/ ${src} ${app} ../tmp/mutants${app.toLowerCase()}/ ../ true`);
-            fs.readdir(`${path}/MDroidPlus/tmp/mutants${app.toLowerCase()}`,function(err, items) {
-                let file;
-                console.log("--------------------> Items: ", items);
-                for(var i=0;i<items.length;i++){
-                    if(items[i].includes('log')){
-                        file = items[i];
-                        break;
-                    }
-                }
-                const content = fs.readFileSync(`${path}/MDroidPlus/tmp/mutants${app.toLowerCase()}/${file}`);
-                s3.saveFileToS3(`${code}`,content,()=>{
-                    for(var i=0;i<items.length;i++){
-                        if(items[i].includes('log')){
-                            fs.unlinkSync(`${path}/MDroidPlus/tmp/mutants${app.toLowerCase()}/${items[i]}`);
+            shell.exec(`cd MDroidPlus/target && java -jar MDroidPlus-1.0.0.jar ../libs4ast/ ${src} ${app} ../tmp/mutants${app.toLowerCase()}/ ../ true`, function(val, stdout, stderr) {
+                    fs.readdir(`${path}/MDroidPlus/tmp/mutants${app.toLowerCase()}/`,function(err, items) {
+                        let file;
+                        for(i=0;i<items.length;i++){
+                            if(items[i].includes('log')){
+                                file = items[i];
+                                break;
+                            }
                         }
-                    }
-                    if(item == itemsEx){
-                        success("ok");
-                    }else{
-                        item = item+1;
-                        resolve();
-                    }
+                        const content = fs.readFileSync(`${path}/MDroidPlus/tmp/mutants${app.toLowerCase()}/${file}`);
+                        s3.saveFileToS3(`${code}`,content,()=>{ 
+                            for(i=0;i<items.length;i++){
+                                if(items[i].includes('log')){
+                                    fs.unlinkSync(`${path}/MDroidPlus/tmp/mutants${app.toLowerCase()}/${items[i]}`);
+                                }
+                            }
+                            if(item == itemsEx){
+                                success("ok");
+                            }else{
+                                item = item+1;
+                                resolve();
+                            }
+                        });
+                    });
                 });
             });
         }));
     }
 }
+
+function requestcall(path_project,code,req,codeinit,itemsEx) {
+    return new Promise(function(resolve, reject) {
+            if(itemsEx>1){
+                let insert = "INSERT INTO `hangover`.`EXECUTION_TESTS` (`code`, `id_application`, `type_application_name`, `level_name`, `type_name`, `type_execution_name`, `number_executions`, `execution_time`, `repetitions`, `status`,`parent`)" 
+                         +  "VALUES ('" + `${code}` + "', '" + req.aplication + "', '" + req.typeAplication + "', '" + req.level + "', '" + req.type + "', '" + req.subType + "', '" + req.numberExecution + "', '" + req.executionTime + "', '" + req.repetitions + "', '" + req.status + "','" + codeinit + "');";
+                console.log(insert);
+                db.query(insert, (err, result) => {
+                    if (err) throw error;
+                    resolve("ok");
+                });
+            }else{
+                resolve("ok");
+            }                
+    }); 
+ }
